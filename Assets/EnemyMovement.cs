a2a61e3b8b689f27b33c89ad8f909d67494247e9
João P. Marques <joaop.marques@outlook.com>
2022-02-26 10:25:56 +0000

    Enemy follows from a given distance radius

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -5,16 +5,23 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] private Transform movePositionTransform;
+  [SerializeField] private Transform followTarget;
 
-    private NavMeshAgent navMeshAgent;
+  public float enemyDistance = 5.0f;
 
-    private void Awake() {
-        navMeshAgent = GetComponent<NavMeshAgent>();
-    }
+  private NavMeshAgent agent;
 
-    void Update()
+  private void Awake()
+  {
+    agent = GetComponent<NavMeshAgent>();
+  }
+
+  void Update()
+  {
+    float distance = Vector3.Distance(transform.position, followTarget.transform.position);
+    if (distance < enemyDistance)
     {
-        navMeshAgent.destination = movePositionTransform.position;
+      agent.SetDestination(followTarget.position);
     }
+  }
 }
